5b6f5b0d9b409d81af3e597433dc3081a505d8a8
vdupham <vdupham@gmail.com>
2024-07-05 12:29:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('api/products.json')
+    fetch('/api/products.json')
         .then(response => response.json())
         .then(products => {
             const itemList = document.getElementById('item-list');
